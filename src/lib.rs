d74dccaecc5368e7c4b6836f8a7dc4779e8c154d
andreabertanzon <andrea.bertanzon@protonmail.com>
2021-08-05 12:33:48 +0200

    actix_web base
    added code to tinker with actix web and check its basics.
    N.B: problems with rust_analyzer not recognizing imports correctly. IntelliJRust works better

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn health_check(req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check",web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
