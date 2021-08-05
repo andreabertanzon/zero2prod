202b6a826536d7d8ff0469226834bba91148c37b
andreabertanzon <andrea.bertanzon@protonmail.com>
2021-08-05 16:46:20 +0200

    First Integration Test
    The commit adds the first integration test and some tinkering with tokio, random ports and TcpListener

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check(req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server=HttpServer::new(|| {
         App::new()
             .route("/health_check",web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,32 @@
-use zero2prod::main;
+use std::net::TcpListener;
 
-#[test]
-fn dummy_test() {
-    main()
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    let address= spawn_app();
+
+    let client = reqwest::Client::new();
+
+    //Act
+    let response = client
+        .get(&format!("{}/health_check",&address))
+        .send()
+        .await
+        .expect("failed to send request");
+
+    //ASSERT
+    assert!(response.status().is_success());
+    assert_eq!(Some(0),response.content_length())
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // retrieving the port assigned to us by the OS
+
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    //returning the application address to the caller
+    format!("http:127.0.0.1:{}",port)
 }
